14e3589d3c7e7c3101ec1bb8a00d365f7f1600ff
Vanisha Kulsu Mooppen <226742@ust-global.com>
2022-08-11 09:05:30 +0530

    Added logger and changed repository to mongoTemplate

--- src/main/java/com/project/onboarding/controller/OnboardingStatusController.java
+++ src/main/java/com/project/onboarding/controller/OnboardingStatusController.java
@@ -22,29 +22,46 @@ import com.project.onboarding.model.StatusReport;
 import com.project.onboarding.model.ResponsePayLoad;
 import com.project.onboarding.service.OnboardingStatusService;
 
+/**
+ * @author Vanisha Kulsu Mooppen
+ * @description : Controller for OnboardingStatus module
+ * @date : 10 August 2022
+ */
+
 @RestController
 @RequestMapping("api/v1")
 public class OnboardingStatusController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(OnboardingStatusController.class);
 
 	@Autowired
 	OnboardingStatusService onboardingStatusReport;
-	
+
+	/**
+	 * @param projectId, userId
+	 * @return ResponseEntity<ResponsePayLoad>
+	 * @description : Preview status report of a particular user for a project
+	 */
 	@GetMapping("/previewReport/{projectId}/{userId}")
-	public ResponseEntity<ResponsePayLoad> getPreviewStatusReport(@Valid @PathVariable("projectId") @Size(min = 1 , message = "Invalid id, please provide valid id") String projectId, 
+	public ResponseEntity<ResponsePayLoad> getPreviewStatusReport(
+			@Valid @PathVariable("projectId") @Size(min = 1, message = "Invalid id, please provide valid id") String projectId,
 			@PathVariable("userId") @Size(min = 1, message = "Invalid id, please provide valid id") String userId) {
 		try {
 			logger.info("In preview status report controller");
+			
 			StatusReport statusReport = onboardingStatusReport.getPreviewStatusReport(projectId, userId);
 			List<Object> statusReportObject = new ArrayList<Object>();
 			statusReportObject.add(statusReport);
+			
 			logger.info("Status report is returned successfully in controller");
-			return new ResponseEntity<ResponsePayLoad>(new ResponsePayLoad(statusReportObject, ProjectOnboardingConstant.API_GET_PREVIEW_REPORT_SUCCESS, ""), HttpStatus.OK);
+			return new ResponseEntity<ResponsePayLoad>(new ResponsePayLoad(statusReportObject,
+					ProjectOnboardingConstant.API_GET_PREVIEW_REPORT_SUCCESS, ""), HttpStatus.OK);
+			
 		} catch (ProjectOnboardingException ex) {
 			logger.warn("Project/User not found, preview report failed in controller");
-			return new ResponseEntity<ResponsePayLoad>(new ResponsePayLoad(null, "", ex.getErrorMessage()), HttpStatus.NOT_FOUND);
+			return new ResponseEntity<ResponsePayLoad>(new ResponsePayLoad(null, "", ex.getErrorMessage()),
+					HttpStatus.NOT_FOUND);
 		}
-		
+
 	}
 }
--- src/main/java/com/project/onboarding/service/impl/OnboardingStatusServiceImpl.java
+++ src/main/java/com/project/onboarding/service/impl/OnboardingStatusServiceImpl.java
@@ -1,7 +1,9 @@
 package com.project.onboarding.service.impl;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
@@ -11,6 +13,7 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import com.project.onboarding.constants.ProjectOnboardingConstant;
 import com.project.onboarding.exception.ProjectOnboardingException;
@@ -19,65 +22,57 @@ import com.project.onboarding.model.ProjectTaskDetails;
 import com.project.onboarding.model.ProjectTasksOverview;
 import com.project.onboarding.model.StatusReport;
 import com.project.onboarding.model.User;
-import com.project.onboarding.repository.ProjectRepository;
-import com.project.onboarding.repository.UserRepository;
 import com.project.onboarding.service.OnboardingStatusService;
+
+/**
+ * @author Vanisha Kulsu Mooppen
+ * @description : Service class for Onboarding Status module.
+ * @date : 10 August 2022
+ */
 
 @Service
 public class OnboardingStatusServiceImpl implements OnboardingStatusService {
 
 	private static final Logger logger = LoggerFactory.getLogger(OnboardingStatusServiceImpl.class);
 
-	@Autowired
-	ProjectRepository projectRepository;
+	public static final Map<String, Integer> TASK_STATUS_PERCENTAGE;
 
-	@Autowired
-	UserRepository userRepository;
+	static {
+		Map<String, Integer> taskPercentageList = new HashMap<String, Integer>();
+		taskPercentageList.put("Yet to start", 0);
+		taskPercentageList.put("In-progress", 50);
+		taskPercentageList.put("Done", 100);
+		TASK_STATUS_PERCENTAGE = Collections.unmodifiableMap(taskPercentageList);
+	}
 
 	@Autowired
 	MongoTemplate mongoTemplate;
-	
+
+	/**
+	 * @param projectId, userId
+	 * @return StatusReport, preview status report object
+	 * @throws ProjectOnboardingException
+	 * @description : Preview status report of a particular user for a project
+	 */
 	@Override
 	public StatusReport getPreviewStatusReport(String projectId, String userId) {
 		logger.info("In preview report service");
-		
-		Query query = new Query();
-		query.addCriteria(Criteria.where("projectId").is(projectId));
-		List<Project> project = mongoTemplate.find(query, Project.class);
-	//	Project existingProject = projectRepository.findByProjectId(projectId);
-		if (null != project && project.size() > 0) {
-			Project existingProject = project.get(0);
-	//		query.getQueryObject().remove(")
-			User existingUser = userRepository.findByUserId(userId);
-			if (null != existingUser) {
-				StatusReport statusReport = new StatusReport();
-				statusReport.setProjectName(existingProject.getName());
-				statusReport.setProjectDescription(existingProject.getDescription());
-				// List<String> usersOfProject = existingProject.getUserIds();
 
-				ProjectTasksOverview projectTasksOverview = new ProjectTasksOverview();
-				projectTasksOverview.setUserId(userId);
-				projectTasksOverview.setUsername(existingUser.getFirstName() + existingUser.getLastName());
-				List<ProjectTaskDetails> projectTaskDetails = existingUser.getProjectIds().stream()
-						.filter(projectDetails -> projectDetails.getProjectId().equals(projectId))
-						.collect(Collectors.toList());
-				
-				double taskPercentage = 0;
-				if (projectTaskDetails.size() > 0) {
-					HashMap<String, Integer> taskPercentageList = new HashMap<String, Integer>();
-					taskPercentageList.put("Yet to start", 0);
-					taskPercentageList.put("In-progress", 50);
-					taskPercentageList.put("Done", 100);
-					long noOfTasks = projectTaskDetails.get(0).getTasks().stream().count();
-					int sumOfPercentage = projectTaskDetails.get(0).getTasks().stream()
-							.map(task -> taskPercentageList.get(task.getStatus()))
-							.collect(Collectors.summingInt(Integer::intValue));
-					
-					taskPercentage = (sumOfPercentage * 100 / (noOfTasks * 100));
-				}
-				projectTasksOverview.setTaskPercentage(taskPercentage);
-				statusReport.setProjectTasksOverview(projectTasksOverview);
-				
+		Criteria criteria = Criteria.where("projectId").is(projectId);
+		Query query = createQuery(criteria);
+		
+		List<Project> projects = mongoTemplate.find(query, Project.class);
+		if (!CollectionUtils.isEmpty(projects)) {
+			Project existingProject = projects.get(0);
+			criteria = Criteria.where("userId").is(userId);
+			query = createQuery(criteria);
+			
+			List<User> users = mongoTemplate.find(query, User.class);
+			if (!CollectionUtils.isEmpty(users)) {
+				User existingUser = users.get(0);
+
+				StatusReport statusReport = createStatusReport(existingProject, existingUser);
+
 				logger.info("Preview status report successfully returned in service");
 				return statusReport;
 			} else {
@@ -90,4 +85,55 @@ public class OnboardingStatusServiceImpl implements OnboardingStatusService {
 		}
 	}
 
+	/**
+	 * @param criteria
+	 * @return query, created query with given criteria
+	 * @description : Create query based on given criteria
+	 */
+	public Query createQuery(Criteria criteria) {
+		Query query = new Query();
+		query.addCriteria(criteria);
+		return query;
+	}
+
+	/**
+	 * @param project, user
+	 * @return StatusReport, Report of the task status for a user
+	 * @description : Create report object with values
+	 */
+	public StatusReport createStatusReport(Project project, User user) {
+		StatusReport statusReport = new StatusReport();
+		statusReport.setProjectName(project.getName());
+		statusReport.setProjectDescription(project.getDescription());
+
+		ProjectTasksOverview projectTasksOverview = new ProjectTasksOverview();
+		projectTasksOverview.setUserId(user.getUserId());
+		projectTasksOverview.setUsername(user.getFirstName() + " " + user.getLastName());
+		
+		List<ProjectTaskDetails> projectTaskDetails = user.getProjectIds().stream()
+				.filter(projectDetails -> projectDetails.getProjectId().equals(project.getProjectId()))
+				.collect(Collectors.toList());
+		projectTasksOverview.setTaskPercentage(calculateTaskPercentage(projectTaskDetails));
+		statusReport.setProjectTasksOverview(projectTasksOverview);
+		
+		return statusReport;
+	}
+
+	/**
+	 * @param projectTaskDetails, A list of task details for the project
+	 * @return taskPercentage
+	 * @description : Calculate task percentage
+	 */
+	public double calculateTaskPercentage(List<ProjectTaskDetails> projectTaskDetails) {
+		double taskPercentage = 0;
+		if (projectTaskDetails.size() > 0) {
+			long noOfTasks = projectTaskDetails.get(0).getTasks().stream().count();
+			int sumOfPercentage = projectTaskDetails.get(0).getTasks().stream()
+					.map(task -> TASK_STATUS_PERCENTAGE.get(task.getStatus()))
+					.collect(Collectors.summingInt(Integer::intValue));
+
+			taskPercentage = (sumOfPercentage * 100 / (noOfTasks * 100));
+		}
+		return taskPercentage;
+	}
 }
